4450e56c9b20099a23864c30679ba16dc5da3e01
suharbramar <suhar_bramar@yahoo.com>
2022-01-17 07:12:35 +0700

    Initial

---
+++ src/test/java/io/bramcode/movie/movieapigateway/MovieApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bramcode.movie.movieapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
